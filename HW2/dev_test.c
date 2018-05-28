976df168308d2991d2d9379a2c6684c105d3341d
ununbum <tmdgus2871@gmail.com>
2018-05-28 16:22:35 +0900

    modified some app

--- HW2/dev_test.c
+++ HW2/dev_test.c
@@ -20,12 +20,68 @@ int main(int argc,char** argv)
 	int dev;
 	unsigned long retval;
 	unsigned int tmp;
+	unsigned char interval,count;
+	int initial;
+	int i;
+	int cnt=0;
 
-	retval = syscall(378,atoi(argv[1]),atoi(argv[2]),strtol(argv[3],NULL,16));
+	if(argc != 4)
+	{
+		printf("parameters are too many or too less\n");
+		printf("ex) /dev_test time_interval(1~100),count(1~100),initial number\n");
+		return -1;
+	}
+
+	interval = atoi(argv[1]);
+	if(interval < 1 || interval >100)
+	{
+		printf("time interval are 1~100 value\n");
+		printf("please type again\n");
+		return -1;
+	}
+	count = atoi(argv[2]);
+	if(count <1 || count >100)
+	{
+		printf("count are 1~100 value\n");
+		printf("please type again\n");
+		return -1;
+	}
+
+	if(strlen(argv[3])!=MAX_DIGIT)
+	{
+		printf("initial number can has no more or less than 4 Digit\n");
+		printf("please type again\n");
+		return -1;
+	}
+	for(i=0;i<4;i++)
+	{
+		if(argv[3][i] == '0')
+			cnt++;
+	}
+	if(cnt!=3)
+	{
+		printf("this device can make output with 3 zero\n");
+		printf("but your input has less or many zero than 3\n");
+		return -1;
+	}
+	for(i=0;i<4;i++)
+	{
+		if(argv[3][i] != '0')
+			cnt=i;
+	}
+	if(argv[3][cnt]>0x38)
+	{
+		printf("this device can make output to 8 only\n");
+		printf("but your input has bigger input than 8\n");
+		return -1;
+	}
+
+	retval = syscall(378,interval,count,strtol(argv[3],NULL,16));
 
 
 	
 	dev = open(DEVICE,O_RDWR);
+
 	if(dev<0)
 	{
 		printf("Device open error :  %s\n",DEVICE);
